748475e39c47663f54e5498ce2737bac0a8646af
sallen12 <sam.allen@stat.unibe.ch>
2023-08-08 16:24:43 +0200

    add equations to documentation of weighted crps

--- scoringrules/_wcrps.py
+++ scoringrules/_wcrps.py
@@ -11,12 +11,22 @@ def twcrps_ensemble(
     forecasts: Array,
     observations: ArrayLike,
     /,
-    v_func: tp.Callable=lambda x, *args: x,
-    v_funcargs: tuple=(),
+    v_func: tp.Callable = lambda x, *args: x,
+    v_funcargs: tuple = (),
     *,
+    axis: int = -1,
     backend: str = "numba",
 ) -> Array:
     r"""Estimate the Threshold-Weighted Continuous Ranked Probability Score (twCRPS) for a finite ensemble.
+
+    Computation is performed using the ensemble representation of the twCRPS in
+    [Allen et al. (2022)](https://arxiv.org/abs/2202.12732):
+
+    $$ \mathrm{twCRPS}(F_{ens}, y) = \frac{1}{M} \sum_{m = 1}^{M} |v(x_{m}) - v(y)| - \frac{1}{2 M^{2}} \sum_{m = 1}^{M} \sum_{j = 1}^{M} |v(x_{m}) - v(x_{j})|,$$
+
+    where $F_{ens}(x) = \sum_{m=1}^{M} \one \{ x_{m} \leq x \}/M$ is the empirical 
+    distribution function associated with an ensemble forecast $x_{1}, \dots, x_{M}$ with
+    $M$ members, and $v$ is the chaining function used to target particular outcomes.
 
     Parameters
     ----------
@@ -31,11 +41,6 @@ def twcrps_ensemble(
         Additional arguments to the chaining function.
     axis: int
         The axis corresponding to the ensemble. Default is the last axis.
-    sorted_ensemble: bool
-        Boolean indicating whether the ensemble members are already in ascending order.
-        Default is False.
-    estimator: str
-        Indicates the CRPS estimator to be used.
     backend: str
         The name of the backend used for computations. Defaults to 'numba' if available, else 'numpy'.
 
@@ -54,6 +59,7 @@ def twcrps_ensemble(
         observations,
         v_func,
         v_funcargs,
+        axis=axis,
     )
     
 
@@ -61,12 +67,22 @@ def owcrps_ensemble(
     forecasts: Array,
     observations: ArrayLike,
     /,
-    w_func: tp.Callable=lambda x, *args: np.ones_like(x),
-    w_funcargs: tuple=(),
+    w_func: tp.Callable = lambda x, *args: np.ones_like(x),
+    w_funcargs: tuple = (),
     *,
+    axis: int = -1,
     backend: str = "numba",
 ) -> Array:
     r"""Estimate the Outcome-Weighted Continuous Ranked Probability Score (owCRPS) for a finite ensemble.
+    
+    Computation is performed using the ensemble representation of the owCRPS in
+    [Allen et al. (2022)](https://arxiv.org/abs/2202.12732):
+
+    $$ \mathrm{owCRPS}(F_{ens}, y) = \frac{1}{M \bar{w}} \sum_{m = 1}^{M} |x_{m} - y|w(x_{m})w(y) - \frac{1}{2 M^{2} \bar{w}^{2}} \sum_{m = 1}^{M} \sum_{j = 1}^{M} |x_{m} - x_{j}|w(x_{m})w(x_{j})w(y),$$
+
+    where $F_{ens}(x) = \sum_{m=1}^{M} \one \{ x_{m} \leq x \}/M$ is the empirical 
+    distribution function associated with an ensemble forecast $x_{1}, \dots, x_{M}$ with
+    $M$ members, $w$ is the chosen weight function, and $\bar{w} = \sum_{m=1}^{M}w(x_{m})/M$.
 
     Parameters
     ----------
@@ -81,11 +97,6 @@ def owcrps_ensemble(
         Additional arguments to the weight function.
     axis: int
         The axis corresponding to the ensemble. Default is the last axis.
-    sorted_ensemble: bool
-        Boolean indicating whether the ensemble members are already in ascending order.
-        Default is False.
-    estimator: str
-        Indicates the CRPS estimator to be used.
     backend: str
         The name of the backend used for computations. Defaults to 'numba' if available, else 'numpy'.
 
@@ -104,6 +115,7 @@ def owcrps_ensemble(
         observations,
         w_func,
         w_funcargs,
+        axis=axis,
     )
     
     
