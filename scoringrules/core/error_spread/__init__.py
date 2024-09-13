6019452c3f8cd6c97b62cb6100593af8e0e9b751
Francesco Zanetta <62377868+frazane@users.noreply.github.com>
2024-09-13 10:02:38 +0200

    wrap numba imports in try except (#69)

--- scoringrules/core/error_spread/__init__.py
+++ scoringrules/core/error_spread/__init__.py
@@ -1,4 +1,8 @@
-from ._gufunc import _error_spread_score_gufunc as _ess_gufunc
+try:
+    from ._gufunc import _error_spread_score_gufunc as _ess_gufunc
+except ImportError:
+    _ess_gufunc = None
+
 from ._score import error_spread_score as ess
 
 __all__ = ["ess", "_ess_gufunc"]
