dee47fb10feaed6dddab6ba89267d71973a2b463
Francesco Zanetta <zanetta.francesco@gmail.com>
2024-10-19 21:01:56 +0200

    implement quantile score

---
+++ scoringrules/_quantile.py
@@ -0,0 +1,57 @@
+import typing as tp
+
+from scoringrules.backend import backends
+
+if tp.TYPE_CHECKING:
+    from scoringrules.core.typing import Array, ArrayLike, Backend
+
+
+def quantile_score(
+    obs: "ArrayLike",
+    fct: "ArrayLike",
+    alpha: "ArrayLike",
+    backend: "Backend | None" = None,
+) -> "Array":
+    r"""Compute the quantile score for a given quantile level.
+
+    The quantile score (Koenker, R. and G. Bassett, 1978) is defined as
+
+    $$
+        S_{\alpha}(q_{\alpha}, y) = \begin{cases}
+        (1 - \alpha) (q_{\alpha} - y), & \text{if } y \leq q_{\alpha}, \\
+        \alpha (y - q_{\alpha}), & \text{if } y > q_{\alpha}.
+        \end{cases}
+    $$
+
+    where $Y$ is the observed value, $F(Y)$ is the forecast value, and $q$ is the quantile level.
+
+    Parameters
+    ----------
+    obs:
+        The observed values.
+    fct:
+        The forecast values.
+    alpha:
+        The quantile level.
+
+    Returns
+    -------
+    score:
+        The quantile score.
+
+    Examples
+    --------
+    >>> import scoringrules as sr
+    >>> sr.quantile_score(0.3, 0.5, 0.2)
+    0.16
+
+    Raises
+    ------
+    ValueError
+        If the quantile level is not between 0 and 1.
+    """
+    B = backends.active if backend is None else backends[backend]
+    obs, fct, alpha = map(B.asarray, (obs, fct, alpha))
+    if B.any(alpha < 0) or B.any(alpha > 1):
+        raise ValueError("The quantile level must be between 0 and 1.")
+    return (B.asarray((obs < fct), dtype=alpha.dtype) - alpha) * (fct - obs)
---
+++ tests/test_quantile.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pytest
+
+import scoringrules as sr
+
+from .conftest import BACKENDS
+
+
+@pytest.mark.parametrize("backend", BACKENDS)
+def test_quantile_score(backend):
+    obs = np.array([0.1, 0.2, 0.3, 0.4, 0.5])
+    fct = np.array([0.1, 0.2, 0.3, 0.4, 0.5])
+    alpha = np.array([0.1, 0.2, 0.3, 0.4, 0.5])
+    res = sr.quantile_score(obs, fct, alpha, backend=backend)
+    res.__class__.__module__ == backend
+
+    # correctness
+    np.isclose(sr.quantile_score(0.3, 0.5, 0.2, backend=backend), 0.16)
+    np.isclose(sr.quantile_score(0.3, 0.1, 0.2, backend=backend), 0.04)
+
+    # value checks
+    with pytest.raises(ValueError):
+        sr.quantile_score(0.3, 0.5, -0.2, backend=backend)
