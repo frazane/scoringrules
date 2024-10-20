eae0be794a71f2079a21fad3279e92c8deb6bec9
Francesco Zanetta <zanetta.francesco@gmail.com>
2024-10-21 00:21:38 +0200

    refactoring and fixes to interval scores

--- scoringrules/core/interval/_score.py
+++ scoringrules/core/interval/_score.py
@@ -6,42 +6,36 @@ if tp.TYPE_CHECKING:
     from scoringrules.core.typing import Array, Backend
 
 
-def _interval_score(
+def interval_score(
     obs: "Array",
     lower: "Array",
     upper: "Array",
     alpha: "Array",
-    backend: "Backend" = None,
 ) -> "Array":
     """Winkler or Interval Score for prediction interval PI[lower, upper] and observations."""
-    # We don't need the backend here
-    obs = obs[..., None]
     width = upper - lower
     above = obs > upper
     below = obs < lower
-    W = width + (
-        below * (2 / alpha) * (lower - obs) + above * (2 / alpha) * (obs - upper)
-    )
+    W = width + below * (2 / alpha) * (lower - obs)
+    W += above * (2 / alpha) * (obs - upper)
     return W
 
 
-def _weighted_interval_score(
+def weighted_interval_score(
     obs: "Array",
     median: "Array",
     lower: "Array",
     upper: "Array",
     alpha: "Array",
-    weight_median: "Array",
-    weight_alpha: "Array",
+    w_median: "Array",
+    w_alpha: "Array",
     backend: "Backend" = None,
 ) -> "Array":
     """Weighted Interval Score for prediction interval PI[lower, upper]."""
     B = backends.active if backend is None else backends[backend]
-    K = weight_alpha.shape[0]
-    IS = _interval_score(obs, lower, upper, alpha)
-    WIS = (
-        B.sum(IS * B.expand_dims(weight_alpha, axis=-2), axis=-1)
-        + weight_median * median
-    )
+    K = w_alpha.shape[0]
+    IS = interval_score(obs[..., None], lower, upper, alpha)
+    WIS = B.sum(IS * w_alpha, axis=-1)
+    WIS += w_median * median
     WIS /= K + 1 / 2
-    return WIS
+    return B.squeeze(WIS)
