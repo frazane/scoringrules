bbc248dd5cc72c42eb0861d207645792edb3f812
Francesco Zanetta <francesco.zanetta@meteoswiss.ch>
2026-06-07 01:13:33 +0200

    Export get_namespace from scoringrules.backend

--- scoringrules/backend/__init__.py
+++ scoringrules/backend/__init__.py
@@ -1,3 +1,4 @@
+from .namespace import ArrayAPINamespace, get_namespace
 from .registry import BackendsRegistry
 
 backends = BackendsRegistry()
@@ -7,4 +8,10 @@ def register_backend(backend):
     backends.register_backend(backend)
 
 
-__all__ = ["backends", "BackendsRegistry", "register_backend"]
+__all__ = [
+    "backends",
+    "BackendsRegistry",
+    "register_backend",
+    "get_namespace",
+    "ArrayAPINamespace",
+]
